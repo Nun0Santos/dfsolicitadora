4592cbf6263e7c2a65d657dbf549d27728e589ed
nalmkibb <nuno.a.santos@criticalsoftware.com>
2025-05-28 22:10:28 +0100

    update css

--- docs/js/script.js
+++ docs/js/script.js
@@ -5,6 +5,22 @@ menuToggle.addEventListener('click', () => {
     navLinks.classList.toggle('active');
 });
 
+document.addEventListener("DOMContentLoaded", function () {
+    const header = document.querySelector("header");
+    const navLinks = document.querySelector(".nav-links");
+    const menuToggle = document.querySelector(".menu-toggle");
+
+    function updateMenuTop() {
+        if (header && navLinks) {
+            const height = header.offsetHeight;
+            navLinks.style.top = height + "px";
+        }
+    }
+    updateMenuTop();
+
+    window.addEventListener("resize", updateMenuTop); 
+});
+
 function openEmail() {
     setTimeout(function () {
         window.location.href = "https://mail.google.com/mail/?view=cm&fs=1&to=doferreira09034@osae.pt";
