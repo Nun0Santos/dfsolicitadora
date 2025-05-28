d4e02e935466bb152c8b8ad2702e8b133fc8b323
nalmkibb <nuno.a.santos@criticalsoftware.com>
2025-05-28 21:01:50 +0100

    website: responsive

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,3 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.getElementById('menu-toggle');
+    const navLinks = document.querySelector('.nav-links');
+
+    menuToggle.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    });
+});
 
 function openEmail() {
     setTimeout(function () {
