c63b9f4537bde4f4333bea8756cf6a37aa79f242
nalmkibb <nuno.a.santos@criticalsoftware.com>
2025-05-28 22:40:33 +0100

    fix js

--- docs/js/script.js
+++ docs/js/script.js
@@ -2,11 +2,12 @@ document.addEventListener("DOMContentLoaded", function () {
     const header = document.querySelector("header");
     const navLinks = document.querySelector(".nav-links");
     const menuToggle = document.getElementById("menu-toggle");
+    const navLinkItems = document.querySelectorAll(".nav-links a");
 
     function updateMenuTop() {
         if (header && navLinks) {
             const height = header.offsetHeight;
-            const offset = 30;  // sobe o menu 20px em relação à base do header
+            const offset = 31;  // sobe o menu 30px em relação à base do header
             navLinks.style.top = (height - offset) + "px";
         }
     }
@@ -16,5 +17,12 @@ document.addEventListener("DOMContentLoaded", function () {
 
     menuToggle.addEventListener("click", () => {
         navLinks.classList.toggle("active");
+    });
+
+    // Fecha o menu quando clicas numa opção do menu
+    navLinkItems.forEach(link => {
+        link.addEventListener("click", () => {
+            navLinks.classList.remove("active");
+        });
     });
 });
